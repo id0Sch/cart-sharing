1c2aead93944f23f0a8cac30cd430cfd2d3f9de0
Ido Schachter <ido.schachter@ironsrc.com>
2015-12-29 17:27:10 +0200

    fix

--- ext/config.js
+++ ext/config.js
@@ -2,8 +2,8 @@
  * Created by idoschachter on 23/12/2015.
  */
 var config = {
-    //server: 'http://52.53.228.12:3001/',
-    server: 'http://localhost:3001/',
+    server: 'http://52.53.228.12:3001/',
+    //server: 'http://localhost:3001/',
     peerjs: {
         key: "zhcd5fbzto0cc8fr",
         secure: false
